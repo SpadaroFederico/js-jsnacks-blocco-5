7648fc733f96590103f5e9bd8ed56faae8bef948
Federico Spadaro <federicospadaro2@gmail.com>
2025-01-22 16:24:49 +0100

    snack 6

---
+++ ex-js-array-methods-main/snack-6/script.js
@@ -0,0 +1,33 @@
+const zucchine = [
+  { type: 'Napoletana', weight: 10, length: 4 },
+  { type: 'Trombetta', weight: 13, length: 16 },
+  { type: 'Napoletana', weight: 4, length: 23 },
+  { type: 'Trombetta', weight: 11, length: 6 },
+  { type: 'Napoletana', weight: 2, length: 17 },
+  { type: 'Romana', weight: 5, length: 10 },
+  { type: 'Romana', weight: 7, length: 9 },
+  { type: 'Trombetta', weight: 3, length: 8 },
+  { type: 'Calabrese', weight: 6, length: 27 },
+  { type: 'Calabrese', weight: 14, length: 4 },
+];
+
+// A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const ZucLunghe = zucchine.filter ((zucchina) => {
+
+  return zucchina.lenght > 15;
+
+});
+
+console.log(ZucLunghe);
+
+
+// verifico la condizione per la lunghezza creando un nuovo array
+
+const AncoraZucchine = zucchine.flter ((zucchina) => {
+
+  return zucchina.lenght < 15
+});
+
+console.log(AncoraZucchine);
+
